c4833ec770e5bf123d4775bf450aac1d155dcaf5
m-ashjaei <mashjaee@gmail.com>
2014-05-14 10:24:12 +0200

    delete

--- SEtSim rev3.0/get_head.m
+++
@@ -1,8 +0,0 @@
-%---------------------------------------------------------------------
-% Copyright 2013 Mohammad Ashjaei <mohammad.ashjaei@mdh.se>
-% This code is written in order to simulate switched Ethernet protocols.
-%---------------------------------------------------------------------
-
-% get the first element of a queue
-function msg = get_head(queue)
-msg = queue(1, 1);
